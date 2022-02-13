5133fc902aa6a21c363305e755d24fc2faccfef5
Ben Fitzhardinge <ben@fitzhardinge.net>
2022-02-13 13:24:09 +0800

    auto gen date

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.forms import ModelForm
+from django.utils import timezone, dateformat
 from .models import NewsStory
 
 
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'pub_date', 'content']
-        widgets = {'pub_date': forms.DateInput(format=('%m/%d/%Y'),attrs={'class':'form-control', 'placeholder':'Select a date','type':'date'}),
+        fields = ["title", "pub_date", "content"]
+        widgets = {
+            "pub_date": forms.DateInput(
+                format=("%Y-%m-%d"),
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Select a date",
+                    "type": "date",
+                    "value": lambda: dateformat.format(timezone.now(), "Y-m-d"),
+                },
+            ),
         }
